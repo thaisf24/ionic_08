348684e3a29c860835ab9068aec2a76ea5e8865f
Thais <42677935+thaisf24@users.noreply.github.com>
2018-11-04 22:23:47 -0200

    Update popularidade.ts

--- src/pages/popularidade/popularidade.ts
+++ src/pages/popularidade/popularidade.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MovieProvider } from '../../providers/movie/movie';
 
-/**
- * 
- * Nome: Claudemir Cardoso RA:816155452
- */
 
 @IonicPage()
 @Component({
